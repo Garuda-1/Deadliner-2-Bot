69c8175e037ce8338a8afb2d98e316d28e4c4ea3
Ian <yan.dolganskiy@mail.ru>
2021-12-07 13:32:11 +0300

    Add basic bot
    Implemented default bot as a base for further development.

--- src/main/java/ru/itmo/sd/deadliner2bot/Deadliner2BotApplication.java
+++ src/main/java/ru/itmo/sd/deadliner2bot/Deadliner2BotApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Deadliner2botApplication {
+public class Deadliner2BotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Deadliner2botApplication.class, args);
+        SpringApplication.run(Deadliner2BotApplication.class, args);
     }
-
 }
